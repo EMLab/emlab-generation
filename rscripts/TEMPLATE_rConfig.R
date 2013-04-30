0d9d9a7eb9d47fbc9921da9e40e2f8f28434944c
JCRichstein <j.c.richstein@tudelft.nl>
2013-04-30 12:25:18 +0200

    Updated rConfig template.

--- rscripts/TEMPLATE_rConfig.R
+++ rscripts/TEMPLATE_rConfig.R
@@ -2,6 +2,7 @@ user<-
 emlabGenerationFolder<-paste("/home/",user,"/emlab-generation/", sep="")
 rscriptFolder<-paste(emlabGenerationFolder,"rscripts/", sep="")
 resultFolder<-paste("/home/",user,"/Desktop/emlabGen/output/", sep="")
+analysisFolder<-paste("/home/", user, "/Desktop/emlabGen/analysis/", sep="")
 queryFile<-paste(emlabGenerationFolder,"emlab-generation/","queries.properties", sep="")
 agentSpringReader<-paste(rscriptFolder,"AgentSpringHeadlessReader.R")
 #agentSpringReader<-paste(rscriptFolder,"AgentSpringHeadlessReaderPureR.R")
