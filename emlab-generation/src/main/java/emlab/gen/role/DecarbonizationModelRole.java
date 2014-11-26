7808c129b02635f8661832af2ed0b83b0e6538d9
JCRichstein <j.c.richstein@tudelft.nl>
2014-11-26 16:33:11 +0100

    Made realRenewable optional. Added files

--- emlab-generation/src/main/java/emlab/gen/role/DecarbonizationModelRole.java
+++ emlab-generation/src/main/java/emlab/gen/role/DecarbonizationModelRole.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2012 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -59,9 +59,9 @@ import emlab.gen.role.operating.PayOperatingAndMaintainanceCostsRole;
 
 /**
  * Main model role.
- * 
+ *
  * @author alfredas, ejlchappin, jcrichstein
- * 
+ *
  */
 @ScriptComponent
 public class DecarbonizationModelRole extends AbstractRole<DecarbonizationModel> implements Role<DecarbonizationModel> {
@@ -140,7 +140,8 @@ public class DecarbonizationModelRole extends AbstractRole<DecarbonizationModel>
         timer.start();
 
         logger.warn("  0a. Determing load duration curves.");
-        determineResidualLoadCurve.act(model);
+        if (model.isRealRenewableDataImplemented())
+            determineResidualLoadCurve.act(model);
 
         logger.warn("  0. Dismantling & paying loans");
         for (EnergyProducer producer : reps.genericRepository.findAllAtRandom(EnergyProducer.class)) {
