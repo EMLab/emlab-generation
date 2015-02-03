9fb9dcf7f9bfd76617327ee1b470e0ebdf26e9f8
JCRichstein <j.c.richstein@tudelft.nl>
2015-02-03 17:46:38 +0100

    Worked on financial reports function.

--- emlab-generation/src/main/java/emlab/gen/role/market/CreatingFinancialReports.java
+++ emlab-generation/src/main/java/emlab/gen/role/market/CreatingFinancialReports.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2013 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,8 +17,6 @@ package emlab.gen.role.market;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import agentspring.role.AbstractRole;
-import agentspring.role.Role;
 import agentspring.role.RoleComponent;
 import emlab.gen.domain.agent.DecarbonizationModel;
 import emlab.gen.domain.market.electricity.FinancialPowerPlantReport;
@@ -31,34 +29,39 @@ import emlab.gen.repository.Reps;
  * Creating finanical reports for each power plant. Currently implemented for
  * use with spot markets only. Long-term contracts are ignored since costs can
  * not easily be assigned to individual power plants.
- * 
+ *
  * @author Joern
- * 
+ *
  */
 @RoleComponent
-public class CreatingFinancialReports extends AbstractRole<DecarbonizationModel> implements
-Role<DecarbonizationModel> {
+public class CreatingFinancialReports extends AbstractClearElectricitySpotMarketRole<DecarbonizationModel> {
 
     @Autowired
     private Reps reps;
 
 
-    @Override
     public void act(DecarbonizationModel model) {
 
         for (PowerPlant plant : reps.powerPlantRepository.findOperationalPowerPlants(getCurrentTick())) {
 
             FinancialPowerPlantReport financialPowerPlantReport = new FinancialPowerPlantReport();
+            financialPowerPlantReport.setFixedCosts(0);
+            financialPowerPlantReport.setFullLoadHours(0);
+            financialPowerPlantReport.setPowerPlant(plant);
+            financialPowerPlantReport.setSpotMarketRevenue(0);
+            financialPowerPlantReport.setVariableCosts(0);
             financialPowerPlantReport.persist();
 
+            // Determining variable costs in current time step.
             double totalSupply = plant.calculateElectricityOutputAtTime(getCurrentTick(), false);
 
             for (SubstanceShareInFuelMix share : plant.getFuelMix()) {
 
                 double amount = share.getShare() * totalSupply;
                 Substance substance = share.getSubstance();
-                findLastKnownPriceForSubstance
-
+                double substanceCost = findLastKnownPriceForSubstance(substance) * amount;
+                financialPowerPlantReport
+                .setVariableCosts(financialPowerPlantReport.getVariableCosts() + substanceCost);
 
             }
         }
