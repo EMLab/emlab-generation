b72795b230682dd21cbb5d60ac3483bbfb26705b
rob <rjjdejeu@gmail.com>
2015-06-25 23:23:33 +0200

    tender clearing updated & payment role started

--- emlab-generation/src/main/java/emlab/gen/role/tender/OrganizingPaymentsFromTenderRole.java
+++ emlab-generation/src/main/java/emlab/gen/role/tender/OrganizingPaymentsFromTenderRole.java
@@ -15,10 +15,63 @@
  ******************************************************************************/
 package emlab.gen.role.tender;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.neo4j.support.Neo4jTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+import emlab.gen.domain.agent.Regulator;
+import emlab.gen.domain.contract.CashFlow;
+import emlab.gen.domain.market.electricity.ElectricitySpotMarket;
+import emlab.gen.domain.market.electricity.PowerPlantDispatchPlan;
+import emlab.gen.domain.market.electricity.SegmentLoad;
+import emlab.gen.domain.policy.renewablesupport.RenewableSupportScheme;
+import emlab.gen.domain.policy.renewablesupport.SupportPriceContract;
+import emlab.gen.domain.technology.PowerGeneratingTechnology;
+import emlab.gen.domain.technology.PowerPlant;
+import emlab.gen.repository.Reps;
+
 /**
  * @author rjjdejeu
  *
  */
-public class OrganizingPaymentsFromTenderRole {
+public class OrganizingPaymentsFromTenderRole extends RenewableSupportScheme {
+    
+    @Transient
+    @Autowired
+    Reps reps;
+
+    @Transient
+    @Autowired
+    Neo4jTemplate template;
+
+    @Transactional
+    public void act(Regulator regulator) {
+
+        SupportPriceContract contract = null;
+
+        //if
+        if (investor.getbidPrice <= getClearingPoint.getCurrentTick()) {
+                    contract = new SupportPriceContract();
+                    contract.setStart(getCurrentTick());
+                }
+
+//        IF (bidPrice of investor < clearingPrice ) {
+//
+//            Contract contract = new Contract();
+//            
+//            contractLength = 15;
+//            contractPrice = clearingPrice of current tick;
+//            contractQuantity = bidQuantity;
+//            yearlyContractPayment = contractPrice * contractQuantity;
+            
+        }
+
+        }
+    }
+}
 
 }
