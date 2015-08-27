593562c0e2a27ea4f045058baea391976e0743e4
Kaveri3012 <kaveri.kariappa.i@gmail.com>
2015-08-27 17:39:09 +0200

    changes in scenario files

---
+++ shellscripts/headlessScripts/KaveriRuns.sh
@@ -0,0 +1,4 @@
+./localHeadlessSingle.sh FipRun1 K_S_FipOne
+./localHeadlessSingle.sh FipRun1point5 K_S_FipOnePointFive
+./localHeadlessSingle.sh FipRun2 K_S_FipTwo
+./localHeadlessSingle.sh FipRun10 K_S_FipTen
